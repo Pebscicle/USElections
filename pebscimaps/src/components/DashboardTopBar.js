467728ea78f6130f1153ef811adb6590fc065a01
Paul Davis <paul.davis438@gmail.com>
2024-06-21 00:52:07 +0200

    Added Alpha version of World Map

---
+++ pebscimaps/src/components/DashboardTopBar.js
@@ -0,0 +1,13 @@
+
+function DashboardTopBar( {children} ) {
+    
+    return (
+        <div style={{backgroundColor: 'orange', display: 'flex'}}>
+            TOP BAR
+            {children}
+        </div>
+    );
+  }
+
+export default DashboardTopBar;
+
