09cd07a95a8c0ada7aadb3dee4ba636fec383c1e
Paul Davis <paul.davis438@gmail.com>
2024-06-20 21:23:04 +0200

    Added highlights to selected states on usa general page.

--- pebscimaps/src/app/usa/page.js
+++ pebscimaps/src/app/usa/page.js
@@ -1,15 +1,40 @@
+'use client';
+
 import CountryInfo from "../../components/CountryInfo"
 import usa from '../../data/usa.json'
 import Link from 'next/link';
 import USAMap from "./USAMap";
 
+import {useState} from 'react';
 
 function USA() {
+
+  const [selectedState, setSelectedState] = useState([]);
+
+  //Function to handle adding/removing states from list
+  const modifyStateFromList = (state) => {
+    let updatedList;
+  
+    // Check if the list is empty
+    if (selectedState.length === 0) {
+      updatedList = [state]; // Add the new state if the list is empty
+    } else if (selectedState.length === 1) {
+      // If the list has exactly one state, replace it with the new state
+      updatedList = [state];
+    } else {
+      // If the list has more than one state, clear it and add the new state
+      updatedList = [];
+    }
+  
+    setSelectedState(updatedList);
+  }
+
+
   return (
     <div style={{color: 'black'}}>
       <div style={{display: 'flex', justifyContent: 'center', height: '100vh'}}>
         <CountryInfo country={usa}>
-          <USAMap infoType={"general"}/>
+          <USAMap infoType={"general"} callbackData={modifyStateFromList} suppliedList={selectedState} />
         </CountryInfo>
       </div>
       {usa.states.map((state) => (
