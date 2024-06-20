c0fb822463fa3a73bc021395e04171f852f04c26
Paul Davis <paul.davis438@gmail.com>
2024-06-20 17:36:31 +0200

    Started adding flags, HDI...

--- pebscimaps/src/components/InfoModal.js
+++ pebscimaps/src/components/InfoModal.js
@@ -8,6 +8,8 @@ import colors from '../resources/colors.json'
 
 import {useEffect, useState} from 'react'
 
+import Link from 'next/link';
+
 function InfoModal( {infoType, selectedYear, isVisible, closeModal, entity, imageLink} ) {
 
   const [isLoading, setIsLoading] = useState(true);
@@ -30,37 +32,62 @@ function InfoModal( {infoType, selectedYear, isVisible, closeModal, entity, imag
     <>
     {isVisible && infoType === "general" &&
       <div style={{position: 'fixed', right: '50px', bottom: '25vh', width: '350px', height: '450px', borderRadius: '5px', color: 'black', backgroundColor: colors.white, boxShadow: "0px 2px 5px 2px gray"}}> 
-        <div style={{display: 'grid', gridTemplateColumns: '1fr auto'}}>
-            <span style={{padding: '2px'}}>{entity.name}</span>
+        <div style={{display: 'grid', gridTemplateColumns: 'auto 50px 1fr auto', gridTemplateRows: '25px'}}>
+            <Link href={`/usa/${entity.name.toLowerCase()}`}>
+              <span style={{color: colors.clickable, fontWeight: 900, padding: '2px', cursor: 'pointer'}}>{entity.name}</span>    
+            </Link>
+            <div style={{paddingLeft: '4px', display: 'flex', alignItems: 'center'}}>
+              <img src={entity.flag} style={{height: '20px'}} alt='Flag'/>
+            </div>
+            <span></span>
             <span style={{cursor: 'pointer', backgroundColor: colors.close, borderRadius: '0px 5px 0px 0px', padding: '0px 8px', display: 'flex', alignItems: 'center'}}onClick={closeModal}>X</span>
         </div>
         <img src={imageLink} style={{ backgroundColor: 'gray', height: '200px', width: '100%', opacity: isLoading? 0.5 : 1 }} alt="placeholder image" height="200px" />
         <div style={{paddingLeft: '4px', paddingTop: '8px'}}>
-          <p>Population: {commaifyNumber(entity.population)}</p>
-          <p>Capital: {entity.capital.name} <span style={{fontSize: 'small', cursor: 'pointer', borderBottom: 'dotted 1px black'}} title={`${Math.round(entity.capital.population/entity.population*100)}% of state's population`}>({commaifyNumber(entity.capital.population)})</span></p>
-          <p>Largest City: {entity.largestCity.name} <span style={{fontSize: 'small', cursor: 'pointer', borderBottom: 'dotted 1px black'}} title={`${Math.round(entity.largestCity.population/entity.population*100)}% of state's population`}>({commaifyNumber(entity.largestCity.population)})</span></p>
-          <p>Area: {isInKM && commaifyNumber(entity.area)}{!isInKM && commaifyNumber(Math.round(convertKM2toMI2(entity.area) ))} {isInKM && <span>km<sup style={{fontSize: 'small'}}>2</sup></span>}{!isInKM && <span>sq mi</span>}
+          <p><span style={{fontWeight: 'bold', color: '#333939'}}>Population</span>: {commaifyNumber(entity.population)}</p>
+          <p><span style={{fontWeight: 'bold', color: '#333939'}}>Capital</span>: {entity.capital.name} <span style={{fontSize: 'small', cursor: 'pointer', borderBottom: 'dotted 1px black'}} title={`${Math.round(entity.capital.population/entity.population*100)}% of state's population`}>({commaifyNumber(entity.capital.population)})</span></p>
+          <p><span style={{fontWeight: 'bold', color: '#333939'}}>Largest City</span>: {entity.largestCity.name} <span style={{fontSize: 'small', cursor: 'pointer', borderBottom: 'dotted 1px black'}} title={`${Math.round(entity.largestCity.population/entity.population*100)}% of state's population`}>({commaifyNumber(entity.largestCity.population)})</span></p>
+          <p><span style={{fontWeight: 'bold', color: '#333939'}}>Area</span>: {isInKM && commaifyNumber(entity.area)}{!isInKM && commaifyNumber(Math.round(convertKM2toMI2(entity.area) ))} {isInKM && <span>km<sup style={{fontSize: 'small'}}>2</sup></span>}{!isInKM && <span>sq mi</span>}
             <span onClick={() => setIsInKM(!isInKM)} style={{color: 'blue', fontSize: 'small', cursor: 'pointer'}}> Swap Unit</span>
           </p>
-          <p>Population Density: {isInKM && Math.round(entity.population/entity.area*100)/100}{!isInKM && Math.round(entity.population/convertKM2toMI2(entity.area)*100)/100}
+          <p><span style={{fontWeight: 'bold', color: '#333939'}}>Population Density</span>: {isInKM && Math.round(entity.population/entity.area*100)/100}{!isInKM && Math.round(entity.population/convertKM2toMI2(entity.area)*100)/100}
             {isInKM && <span>/km<sup style={{fontSize: 'small'}}>2</sup></span>}
             {!isInKM && <span>/sq mi</span>}
             <span onClick={() => setIsInKM(!isInKM)} style={{color: 'blue', fontSize: 'small', cursor: 'pointer'}}> Swap Unit</span>
           </p>
-          <p>Nominal GDP in billions: ${commaifyNumber(entity.gdp)}</p>
-          <p>GDP per capita: ${commaifyNumber( parseInt(entity.gdp/entity.population*1000000) )}</p>
+          <p><span style={{fontWeight: 'bold', color: '#333939'}}>Nominal GDP in billions</span>: ${commaifyNumber(entity.gdp)}</p>
+          <p><span style={{fontWeight: 'bold', color: '#333939'}}>GDP per capita</span>: ${commaifyNumber( parseInt(entity.gdp/entity.population*1000000) )}</p>
+          <p><span style={{fontWeight: 'bold', color: '#333939'}}>HDI</span>: <span>
+            {entity.hdi} <span
+            style={{
+              color: entity.hdi >= 0.8? '#00441B' : 
+              (entity.hdi > 0.699 ? '#66C2A4' : 
+                (entity.hdi > 0.549 ? '#b2b849' :
+                  '#6D0026'
+                )
+              )
+            }}>(
+              {entity.hdi >= 0.8 && <span>Very High</span>}
+              {(entity.hdi < 0.8 && entity.hdi > 0.699)  && <span>High</span>}
+              {(entity.hdi < 0.700 && entity.hdi > 0.549)  && <span>Medium</span>}
+              {entity.hdi < 0.550 && <span>Low</span>}
+              )</span>
+            </span>
+          </p>
         </div>
       </div>
     }
     {isVisible && infoType === "elections" &&
       <div style={{position: 'fixed', right: '50px', bottom: '25vh', width: '350px', height: '450px', borderRadius: '5px', color: 'black', backgroundColor: colors.white, boxShadow: "0px 2px 5px 2px gray"}}> 
-        <div style={{display: 'grid', gridTemplateColumns: '1fr auto'}}>
-            <span>{entity.name}
-              <span style={{backgroundColor: {determineBackgroundColor}, color: 'white', borderRadius: '5px', paddingLeft: '2px', paddingRight: '2px', marginLeft: '4px'}}>
-                  R+4
-              </span>
-            </span>
-            <span style={{cursor: 'pointer', backgroundColor: colors.close, borderRadius: '0px 5px 0px 0px', padding: '0px 8px'}}onClick={closeModal}>X</span>
+        <div style={{display: 'grid', gridTemplateColumns: 'auto 50px 1fr auto', gridTemplateRows: '25px'}}>
+            <Link href={`/usa/${entity.name.toLowerCase()}`}>
+              <span style={{padding: '2px', cursor: 'pointer'}}>{entity.name}</span>    
+            </Link>
+            <div style={{paddingLeft: '4px', display: 'flex', alignItems: 'center'}}>
+              <img src={entity.flag} style={{height: '20px'}} alt='Flag'/>
+            </div>
+            <span></span>
+            <span style={{cursor: 'pointer', backgroundColor: colors.close, borderRadius: '0px 5px 0px 0px', padding: '0px 8px', display: 'flex', alignItems: 'center'}}onClick={closeModal}>X</span>
         </div>
         <img src={imageLink} style={{ backgroundColor: 'gray', height: '200px', width: '100%', opacity: isLoading? 0.5 : 1 }} alt="placeholder image" height="200px" />
         <div style={{paddingLeft: '4px', paddingTop: '8px'}}>
