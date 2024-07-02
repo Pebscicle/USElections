f67cd6d740e1fb8f8c157cdd7cbb0242f0ea0b98
Paul Davis <paul.davis438@gmail.com>
2024-07-02 17:21:43 +0200

    ....

--- pebscimaps/next.config.mjs
+++ pebscimaps/next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true, // Optional: Add this if you haven't already
+    // Other Next.js configurations...
+    experimental: {
+      missingSuspenseWithCSRBailout: false, // The option you want to add
+    },
 };
 
 export default nextConfig;
