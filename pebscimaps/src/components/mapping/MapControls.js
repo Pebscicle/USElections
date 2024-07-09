b6c5b38b5fd48adfaa14c2db924e4a21eb275ae0
Paul Davis <paul.davis438@gmail.com>
2024-07-09 22:02:07 +0200

    adjustment

--- pebscimaps/src/components/mapping/MapControls.js
+++ pebscimaps/src/components/mapping/MapControls.js
@@ -6,14 +6,14 @@ function MapControls( {right, bottom, left, top, movementDetected} ) {
             <small style={{display: 'flex', justifyContent: 'center', borderBottom: '1px solid black'}}>Map Controls</small>
             <div style={{display: 'grid', width: '100px', height: '100px', 
                 gridTemplateRows: '1fr 1fr 1fr', gridTemplateColumns: '1fr 1fr 1fr'}}>
-                <span></span>
+                <button onClick={() => movementDetected({'x': 1, 'y': 1})} style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}} ><img src={'https://img.icons8.com/?size=100&id=717&format=png&color=000000'} width={25} height={25} alt='Zoom Out' /></button>
                 <button onClick={() => movementDetected({'x': 0, 'y': -1})}><img src={'https://img.icons8.com/?size=100&id=86088&format=png&color=000000'} style={{ transform: 'rotate(-90deg)' }} width={50} height={50} alt='Up' /></button>
-                <span></span>
+                <button onClick={() => movementDetected({'x': -1, 'y': -1})} style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}} ><img src={'https://img.icons8.com/?size=100&id=715&format=png&color=000000'} width={25} height={25} alt='Zoom In' /></button>
                 <button onClick={() => movementDetected({'x': -1, 'y': 0})}><img src={'https://img.icons8.com/?size=100&id=86088&format=png&color=000000'} style={{ transform: 'rotate(180deg)' }} width={50} height={50} alt='Left' /></button>
                 <span></span>
-                <button onClick={() => movementDetected({'x': 0, 'y': 1})}><img src={'https://img.icons8.com/?size=100&id=86088&format=png&color=000000'} style={{ transform: 'rotate(0deg)' }} width={50} height={50} alt='Right' /></button>
+                <button onClick={() => movementDetected({'x': 1, 'y': 0})}><img src={'https://img.icons8.com/?size=100&id=86088&format=png&color=000000'} style={{ transform: 'rotate(0deg)' }} width={50} height={50} alt='Right' /></button>
                 <span></span>
-                <button onClick={() => movementDetected({'x': 1, 'y': 0})}><img src={'https://img.icons8.com/?size=100&id=86088&format=png&color=000000'} style={{ transform: 'rotate(90deg)' }} width={50} height={50} alt='Down' /></button>
+                <button onClick={() => movementDetected({'x': 0, 'y': 1})}><img src={'https://img.icons8.com/?size=100&id=86088&format=png&color=000000'} style={{ transform: 'rotate(90deg)' }} width={50} height={50} alt='Down' /></button>
                 <span></span>
             </div>
         </div>
