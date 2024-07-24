d10092923473387083567066ee517ea4c7dd5bad
Paul Davis <paul.davis438@gmail.com>
2024-07-24 13:10:37 +0200

    ?.

--- pebscimaps/src/components/CountryInfo.js
+++ pebscimaps/src/components/CountryInfo.js
@@ -20,15 +20,15 @@ function CountryInfo( {children, country} ) {
           <article className="max-w-full md:max-w-2xl mx-auto pt-8">
 
           <h3 className='py-2'  style={{fontWeight: 'semibold'}}>Summary</h3>
-            <p style={{textAlign: 'justify'}}>{country.article.summary}</p>
+            <p style={{textAlign: 'justify'}}>{country?.article?.summary}</p>
 
           <h3 className='py-2'  style={{fontWeight: 'semibold'}}>Geography</h3>
-            <p style={{textAlign: 'justify'}}>{country.article.geography}</p>
+            <p style={{textAlign: 'justify'}}>{country?.article?.geography}</p>
             <p style={{textAlign: 'justify'}}><span style={{fontWeight: 'bold', color: '#333939'}}>Area</span>: {isInKM && commaifyNumber(country.area)}{!isInKM && commaifyNumber(Math.round(convertKM2toMI2(country.area) ))} {isInKM && <span>km<sup style={{fontSize: 'small'}}>2</sup></span>}{!isInKM && <span>sq mi</span>}
             <span onClick={() => setIsInKM(!isInKM)} style={{color: 'blue', fontSize: 'small', cursor: 'pointer'}}> Swap Unit</span></p>
 
           <h3 className='py-2'  style={{fontWeight: 'semibold'}}>Demographics</h3>
-            <p style={{textAlign: 'justify'}}>{country.article.demographics}</p>
+            <p style={{textAlign: 'justify'}}>{country?.article?.demographics}</p>
             <p style={{textAlign: 'justify'}}><span style={{fontWeight: 'bold', color: '#333939'}}>Population</span>: {commaifyNumber(country.population)}</p>
             <p style={{textAlign: 'justify'}}><span style={{fontWeight: 'bold', color: '#333939'}}>Population Density</span>: {isInKM && Math.round(country.population/country.area*100)/100}{!isInKM && Math.round(country.population/convertKM2toMI2(country.area)*100)/100}
             {isInKM && <span>/km<sup style={{fontSize: 'small'}}>2</sup></span>}
@@ -36,7 +36,7 @@ function CountryInfo( {children, country} ) {
             <span onClick={() => setIsInKM(!isInKM)} style={{color: 'blue', fontSize: 'small', cursor: 'pointer'}}> Swap Unit</span></p>
 
           <h3 className='py-2'  style={{fontWeight: 'semibold'}}>Economy</h3>
-            <p style={{textAlign: 'justify'}}>{country.article.economy}</p>
+            <p style={{textAlign: 'justify'}}>{country?.article?.economy}</p>
             <p style={{textAlign: 'justify'}}><span style={{fontWeight: 'bold', color: '#333939'}}>Nominal GDP in billions</span>: ${commaifyNumber(country.gdp)}</p>
             <p style={{textAlign: 'justify'}}><span style={{fontWeight: 'bold', color: '#333939'}}>GDP per capita</span>: ${commaifyNumber( parseInt(country.gdp/country.population*1000000) )}</p>
             <p style={{textAlign: 'justify'}}>
@@ -48,16 +48,16 @@ function CountryInfo( {children, country} ) {
             </p>
 
           <h3 className='py-2'  style={{fontWeight: 'semibold'}}>Culture</h3>
-            <p style={{textAlign: 'justify'}}>{country.article.culture}</p>
+            <p style={{textAlign: 'justify'}}>{country?.article?.culture}</p>
 
           <h3 className='py-2'  style={{fontWeight: 'semibold'}}>Education</h3>
-            <p style={{textAlign: 'justify'}}>{country.article.education}</p>
+            <p style={{textAlign: 'justify'}}>{country?.article?.education}</p>
 
           <h3 className='py-2'  style={{fontWeight: 'semibold'}}>Infrastructure</h3>
-            <p style={{textAlign: 'justify'}}>{country.article.infrastructure}</p>
+            <p style={{textAlign: 'justify'}}>{country?.article?.infrastructure}</p>
 
           <h3 className='py-2'  style={{fontWeight: 'semibold'}}>Government</h3>
-            <p style={{textAlign: 'justify'}}>{country.article.government}</p>
+            <p style={{textAlign: 'justify'}}>{country?.article?.government}</p>
 
 
           </article>
