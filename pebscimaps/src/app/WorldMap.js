0ddb1db86829a61159251b0bc0e9d301c7eec0c0
Paul Davis <paul.davis438@gmail.com>
2024-08-05 18:13:48 +0200

    Fix...

--- pebscimaps/src/app/WorldMap.js
+++ pebscimaps/src/app/WorldMap.js
@@ -299,7 +299,7 @@ useEffect(() => {
 return (
     <div className='world-map-container' style={{backgroundColor: water, width: '100%', height: {height}, display: 'flex', justifyContent: 'center'}}>
 
-      <MapControls bottom={!isMobile ? 123 : 496} right={0} buttonClicked={handleButtonClick}/>
+      <MapControls bottom={123} right={0} buttonClicked={handleButtonClick}/>
       
 
       {infoType != 'creator' &&
